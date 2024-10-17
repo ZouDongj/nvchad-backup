15c62a5dd9e72a33b6d4643961e87c53f85bbe18
ZouDongj <41882825+ZouDongj@users.noreply.github.com>
2024-10-17 16:37:30 +0800

    Update chadrc.lua

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,24 +1,51 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = {
+M.base46 = {
   theme = "bearded-arc",
 
-  -- hl_override = {
-  --  Comment = { italic = true },
-  --  ["@comment"] = { italic = true },
-  -- },
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+
+    FoldColumn = {
+      bg = "black",
+    },
+  },
+}
+
+M.ui = {
+  statusline = {
+    theme = "minimal",
+    separator_style = "round",
+  },
+
   tabufline = {
     enabled = false,
     lazyload = true,
     overriden_modules = nil,
   },
-  nvdash = {
-    load_on_startup = true,
-  },
+}
+
+M.nvdash = {
+  load_on_startup = true,
+  header = {
+       "                            ",
+       "     ▄▄         ▄ ▄▄▄▄▄▄▄   ",
+       "   ▄▀███▄     ▄██ █████▀    ",
+       "   ██▄▀███▄   ███           ",
+       "   ███  ▀███▄ ███           ",
+       "   ███    ▀██ ███           ",
+       "   ███      ▀ ███           ",
+       "   ▀██ █████▄▀█▀▄██████▄    ",
+       "     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀   ",
+       "                            ",
+       "     Powered By Neovim    ",
+       "                            ",
+     },
 }
 
 return M
