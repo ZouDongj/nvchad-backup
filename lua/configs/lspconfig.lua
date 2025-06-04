e80a9ccd4e3671539ce3fc01694c8f373524e3d0
ZouDongj <41882825+ZouDongj@users.noreply.github.com>
2025-06-04 22:05:07 +0800

    Update lspconfig.lua
    同步更新

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -5,23 +5,18 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
 
-local servers = { "html", "cssls", "ts_ls", "clangd", "pyright", "bashls", "lemminx", "cmake", "lua_ls" }
+local servers = { "html", "cssls", "ts_ls", "clangd", "pyright", "bashls", "lemminx", "cmake" }
 
 local my_on_attach = function(_, bufnr)
   on_attach(_, bufnr)
   -- 设置诊断信息的显示样式
+  local x = vim.diagnostic.severity
   vim.diagnostic.config({
     virtual_text = false,
-    signs = true,
+    signs = { text = { [x.ERROR] = "󰅚 ", [x.WARN] = "󰀪 ", [x.HINT] = "󰌶 ", [x.INFO] = " " } },
     underline = true,
     update_in_insert = false,
   })
-
-  local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
-  for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-  end
 end
 
 -- lsps with default config
@@ -32,6 +27,19 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   })
 end
+
+lspconfig["lua_ls"].setup({
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" }, -- 添加 vim 作为全局变量
+      },
+    },
+  },
+  on_attach = my_on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+})
 
 -- -- typescript
 -- lspconfig.tsserver.setup {
